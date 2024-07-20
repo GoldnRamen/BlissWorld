f3000cc61691c17fa699608af9822ae3ab6c7bf4
Adelson <adelsondabup@gmail.com>
2024-07-20 14:09:08 +0100

    All validations should be complete now. Local storage upload is next

--- js/login.js
+++ js/login.js
@@ -1,85 +1,79 @@
 $(document).ready(function (){
-
-    $('#btn').on('click', function () {
-        let email = $('#email').val();
-        let password = $('#password').val();
-        let emailErr = $('#emailErr');
-        let passwordErr = $('#passwordErr');
-        const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-z0-9.-]+\.[a-zA-Z]{2,4}$/;
-
-        // console.log(email, password)
-
-        if(email == ""){
-            emailErr.text('Email can not be empty')
-        } else if (!emailRegex.test(email)) {
-            emailErr.text('Enter a valid email')
-            
-        }else if(email){
-            emailErr.text('')
-        }
-       if(password == ""){
-        passwordErr.text('Password can not be empty')
-                
-        }else if (password.length < 8){
-            passwordErr.text('Password can not be less than 8')
-        }else
-            passwordErr.text('')
-
-            if(emailErr.text() =="" && passwordErr.text() == ""){
-            
-            location.href = 'index.html'
-            
+    $(document).ready(function() {
+        $("#loginForm").on("submit", function(e) {
+            e.preventDefault();
+    
+            let email = $('#email').val();
+            let password = $('#password').val();
+            let emailErr = $('#emailErr');
+            let passwordErr = $('#passwordErr');
+            const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-z0-9.-]+\.[a-zA-Z]{2,4}$/;
+            let valid = true;
+    
+            // Clear previous error messages
+            emailErr.text('');
+            passwordErr.text('');
+    
+            if (email === "") {
+                emailErr.text('Email cannot be empty');
+                valid = false;
+            } else if (!emailRegex.test(email)) {
+                emailErr.text('Enter a valid email');
+                valid = false;
             }
-        
-
-            // console.log(email, password)
-
-
-        // if(emailErr.text() == "" && passwordErr.text() == "" ){
-        //     location.href = 'login.html'
-
-        
+    
+            if (password === "") {
+                passwordErr.text('Password cannot be empty');
+                valid = false;
+            } else if (password.length < 8) {
+                passwordErr.text('Password cannot be less than 8 characters');
+                valid = false;
+            }
+    
+            if (valid) {
+                // Redirect to the index page if validation passes
+                location.href = 'index.html';
+            }
+        });
     });
+    
+    
 
 
-    $('#merchBtn').on('click', function () {
-        let email2 = $('#merchEmail').val();
-        let password2 = $('#merchPassword').val();
-        let emailErr2 = $('#merchEmailErr');
-        let passwordErr2 = $('merchPasswordErr');
-        const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    $("#merchLogin").on("submit", function(e) {
+        e.preventDefault();
 
-        // console.log(email, password)
+        let valid = true;
+        const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+        
+        // Clear previous error messages
+        $('.error').text('');
 
-        if(email2 == ""){
-            emailErr2.text('Email can not be empty')
-        } else if (!emailRegex.test(email2)) {
-            emailErr2.text('Enter a valid email')
-            
-        }else if(email2){
-            emailErr2.text('')
+        // Validate Email
+        let email = $('#merchEmail').val();
+        if (email === "") {
+            $('#merchEmailErr').text('Email cannot be empty');
+            valid = false;
+        } else if (!emailRegex.test(email)) {
+            $('#merchEmailErr').text('Enter a valid email');
+            valid = false;
         }
-       if(password2 == ""){
-        passwordErr2.text('Password can not be empty')
-                
-        }else if (password2.length < 8){
-            passwordErr2.text('Password can not be less than 8')
-        }else
-            passwordErr2.text('')
 
-            if(emailErr2.text() =="" && passwordErr2.text() == ""){
-            
-            location.href = 'dashboard.html'
-            
-            }
-        
+        // Validate Password
+        let password = $('#merchPassword').val();
+        if (password === "") {
+            $('#merchPasswordErr').text('Password cannot be empty');
+            valid = false;
+        } else if (password.length < 6) {
+            $('#merchPasswordErr').text('Password must be at least 6 characters long');
+            valid = false;
+        }
 
-            // console.log(email, password)
-
-
-        // if(emailErr.text() == "" && passwordErr.text() == "" ){
-        //     location.href = 'login.html'
-
-        
+        // If form is valid, submit the form
+        if (valid) {
+            alert("Form submitted successfully!");
+            this.submit();
+            window.location.href = "dashboard.html"
+        }
     });
 })
--- js/register.js
+++ js/register.js
@@ -1,58 +1,130 @@
 $(document).ready(function(){
-    $('#regBtn').on('click', function () {
-        preventDefault()
-        valid = true
+    $('#registerForm').on('submit', function (e) {
+        e.preventDefault();
+        let valid = true;
 
-        let regFirstname = $('#regFirstname').val();
-        let regFirstnameErr = $("#regFirstnameErr").val();
-        let regSurnameErr = $('#regSurnameErr').val();
-        let regSurname = $('#regSurname').val();
-        let reg_email = $('#regEmail').val();
-        let reg_password = $('#regPassword').val();
-        let reg_emailErr = $('#regEmailErr');
-        let reg_passwordErr = $('#regPasswordErr');
-        const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-z0-9.-]+\.[a-zA-Z]{2,4}$/;
+        const regFirstname = $('#regFirstname').val();
+        const regFirstnameErr = $("#regFirstnameErr");
+        const regSurname = $('#regSurname').val();
+        const regSurnameErr = $('#regSurnameErr');
+        const regEmail = $('#regEmail').val();
+        const regEmailErr = $('#regEmailErr');
+        const regPassword = $('#regPassword').val();
+        const regPasswordErr = $('#regPasswordErr');
+        const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
 
+        // Clear previous error messages
+        $(".error").text('');
 
-        // console.log(email, password)
-        if(regFirstname == ""){
-            valid = false
-            regFirstnameErr.text('Email can not be empty')
+        if(regFirstname === ""){
+            valid = false;
+            regFirstnameErr.text('First Name cannot be empty');
         }
-        if(regSurname == ""){
-            valid = false
-            regSurnameErr.text('Email can not be empty')
+        if(regSurname === ""){
+            valid = false;
+            regSurnameErr.text('Last Name cannot be empty');
         }
-        if(reg_email == ""){
-            valid = false
-            reg_emailErr.text('Email can not be empty')
-        } else if (!emailRegex.test(reg_email)) {
-            valid = false
-            reg_emailErr.text('Enter a valid email')
+        if(regEmail === ""){
+            valid = false;
+            regEmailErr.text('Email cannot be empty');
+        } else if (!emailRegex.test(regEmail)) {
+            valid = false;
+            regEmailErr.text('Enter a valid email');
         }
-       if(reg_password == ""){
-        valid = false
-        reg_passwordErr.text('Password can not be empty')
-                
-        }else if (reg_password.length < 8){
-            valid  = false
-            reg_passwordErr.text('Password can not be less than 8')
+        if(regPassword === ""){
+            valid = false;
+            regPasswordErr.text('Password cannot be empty');
+        } else if (regPassword.length < 8){
+            valid = false;
+            regPasswordErr.text('Password cannot be less than 8 characters');
         }
-        else {
-            $("#registerForm .error").text('')
-            valid = true
-            alert("Registeration is successful. Login now")
-            location.href = 'login.html'
-            }
-        
 
-            // console.log(email, password)
-
-
-        // if(emailErr.text() == "" && passwordErr.text() == "" ){
-        //     location.href = 'login.html'
-
-        
+        if(valid) {
+            alert("Registration is successful. Login now");
+            location.href = 'login.html';
+        }
     });
 
-})
+    $("#merchRegForm").on("submit", function(e) {
+        e.preventDefault();
+
+        let valid = true;
+        const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+        
+        // Clear previous error messages
+        $('.error').text('');
+
+        // Validate First Name
+        let firstName = $('#firstName').val();
+        if (firstName === "") {
+            $('#firstNameError').text('First Name cannot be empty');
+            valid = false;
+        }
+
+        // Validate Last Name
+        let lastName = $('#lastName').val();
+        if (lastName === "") {
+            $('#lastNameError').text('Last Name cannot be empty');
+            valid = false;
+        }
+
+        // Validate Email
+        let email = $('#email').val();
+        if (email === "") {
+            $('#emailError').text('Email cannot be empty');
+            valid = false;
+        } else if (!emailRegex.test(email)) {
+            $('#emailError').text('Enter a valid email');
+            valid = false;
+        }
+
+        // Validate Phone Number
+        let phone = $('#phone').val();
+        if (phone === "") {
+            $('#phoneError').text('Phone Number cannot be empty');
+            valid = false;
+        } else if (!/^\d{11}$/.test(phone)) {
+            $('#phoneError').text('Phone number must be 11 numbers');
+            valid = false;
+        }
+
+        // Validate Store Name
+        let storeName = $('#storeName').val();
+        if (storeName === "") {
+            $('#storeNameError').text('Store Name cannot be empty');
+            valid = false;
+        }
+
+        // Validate Description
+        // let description = $('#description').val();
+        // if (description === "") {
+            
+        //     valid = false;
+        // }
+
+        // Validate Password
+        let password = $('#password').val();
+        let secPassword = $('#secPassword').val();
+        if (password === "") {
+            $('#passwordError').text('Password cannot be empty');
+            valid = false;
+        } else if (password.length < 8) {
+            $('#passwordError').text('Password must be at least 8 characters long');
+            valid = false;
+        }
+        // Validate Retype Password
+        if (secPassword === "") {
+            $('#secPasswordError').text('Retype Password cannot be empty');
+            valid = false;
+        } else if (password !== secPassword) {
+            $('#secPasswordError').text('Passwords do not match');
+            valid = false;
+        }
+
+        // If form is valid, submit the form
+        if (valid) {
+            alert("Registeration is successful. You can login now merchant.")
+            window.location.href = "merchantLog.html"
+        }
+    });
+});   
